c7d4eacb9e475589cdd7f7fbd580980429f8633a
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-13 11:45:03 -0600

    updates

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  resources :users
+  resources :sessions
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get 'welcome/index'
+
+  root 'welcome#index'
+  resources :articles do
+    resources :comments
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
